f67adaaf65c2604d7728799398aa11dd5cc6aea0
Sanket Marathe <sanketmarathe80@gmail.com>
2019-06-28 10:40:09 +0530

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -8,8 +8,8 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "bf5b0034654e483abed840bd9dd54dc0"; //Blynk Server
-//char auth[] = "GwYJ0HazquNBdRWcDUCLUWX6AhfcX7ds";   //Local Server
+char auth[] = "Your Auth Code"; //Blynk Server
+//char auth[] = "Your Auth Code";   //Local Server
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
