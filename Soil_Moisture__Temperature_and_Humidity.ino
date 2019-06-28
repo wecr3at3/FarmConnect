d3ae3e5f545fee5eb19038917d4089e7d8e691e8
Sanket Marathe <sanketmarathe80@gmail.com>
2019-06-28 10:36:27 +0530

    Add files via upload

---
+++ Soil_Moisture__Temperature_and_Humidity.ino
@@ -0,0 +1,80 @@
+
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SPI.h>
+#include <DHT.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "bf5b0034654e483abed840bd9dd54dc0"; //Blynk Server
+//char auth[] = "GwYJ0HazquNBdRWcDUCLUWX6AhfcX7ds";   //Local Server
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Sanket";
+char pass[] = "2444666668888888";
+
+int sensorPin = A0; 
+int sensorValue;  
+int limit = 300; 
+
+#define DHTPIN 8 
+
+#define DHTTYPE DHT11 
+
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);  //V5 is for Humidity
+  Blynk.virtualWrite(V6, t);  //V6 is for Temperature
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  pinMode(13, OUTPUT);
+
+  Blynk.begin(auth, ssid, pass);
+  //Blynk.begin(auth, IPAddress(192,168,43,72), 8080);
+  //Blynk.begin(auth, ssid, pass, "192.168.43.251", 8080);
+  
+  dht.begin();
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+
+  sensorValue = analogRead(sensorPin); 
+ Serial.println("Analog Value : ");
+ Serial.println(sensorValue);
+
+ Blynk.virtualWrite(V4, sensorValue); //V4 is for Soil Moisture
+ 
+ if (sensorValue<limit) {
+ digitalWrite(13, HIGH); 
+ }
+ else {
+ digitalWrite(13, LOW); 
+ }
+
+ timer.run(); // Initiates SimpleTimer
+}
